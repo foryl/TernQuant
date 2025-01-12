ab97d80c13bbb8bf978b8dce984533c42ad510de
Black_Jack <helloworld.jack@outlook.com>
2025-01-12 23:35:13 +0800

    [ADD]修改APP.VUE

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import Echarts from 'vue-echarts'
+import * as echarts from 'echarts'
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.component('e-charts',Echarts)
+app.config.globalProperties.$echarts = echarts
+app.mount("#app");
